b74ba05b7352367afb0f734a8e62af1df5181ae7
Hex <2001phs92@gmail.com>
2023-10-08 00:58:08 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 408 ms, Memory: 134128 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N,M = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(N)]
+dp = [[0]*(N+1) for _ in range(N+1)]
+
+for i in range(N):
+    for j in range(N):
+        dp[i+1][j+1] = graph[i][j]+dp[i+1][j]+dp[i][j+1]-dp[i][j]
+
+for _ in range(M):
+    x1,y1,x2,y2 = map(int, input().split())
+    print(dp[x2][y2]+dp[x1-1][y1-1]-dp[x1-1][y2]-dp[x2][y1-1])
