1eccf3fda794bfd82e67afb62a6aa10d26136822
Hex <2001phs92@gmail.com>
2023-11-27 03:35:41 +0900

    [Gold IV] Title: N-Queen, Time: 30312 ms, Memory: 198968 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -0,0 +1,24 @@
+N = int(input())
+
+ans = 0
+row = [0] * N
+
+def is_promising(x):
+    for i in range(x):
+        if row[x] == row[i] or abs(row[x] - row[i]) == abs(x - i):
+            return False
+    return True
+
+def n_queens(x):
+    global ans
+    if x == N:
+        ans += 1
+        return
+    else:
+        for i in range(N):
+            row[x] = i
+            if is_promising(x):
+                n_queens(x+1)
+
+n_queens(0)
+print(ans)
