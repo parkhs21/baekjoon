3a17ab867090fb7929b5f0fcedbc5968d5b85ce5
Hex <2001phs92@gmail.com>
2024-04-01 13:35:18 +0900

    [Silver I] Title: 절댓값 힙, Time: 352 ms, Memory: 26860 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -12,23 +12,20 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         
-        PriorityQueue<int[]> heap = new PriorityQueue<>(new Comparator<int[]>() {
+        PriorityQueue<Integer> heap = new PriorityQueue<>(new Comparator<Integer>() {
             @Override
-            public int compare(int[] o1, int[] o2) {
-                if(o1[0] < o2[0]) return -1;
-                else if(o1[0] > o2[0]) return 1;
-                else if(o1[1] < o2[1]) return -1;
-                else if(o1[1] > o2[1]) return 1;
-                return 0;
+            public int compare(Integer o1, Integer o2) {
+                if (Math.abs(o1) == Math.abs(o2)) return o1-o2;
+                else return Math.abs(o1)-Math.abs(o2);
             }
         });
 
         while(N-- > 0) {
             int x = Integer.parseInt(br.readLine());
 
-            if(x!=0) heap.add(new int[] {Math.abs(x), x});
+            if(x!=0) heap.add(x);
             else if(heap.size() == 0) sb.append("0\n");
-            else sb.append(heap.poll()[1]+"\n");
+            else sb.append(heap.poll()+"\n");
         }
 
         System.out.print(sb.toString());
